f11524b11b97833567706f1333b822dc43554b6a
“krv006” <rvkamronbek@gmail.com>
2024-06-03 11:28:53 +0500

    last

--- core/urls.py
+++ core/urls.py
@@ -3,9 +3,10 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls.i18n import i18n_patterns
-
+from django.views.i18n import set_language
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('set_language/', set_language, name='set_language')
 ]
 
 urlpatterns += i18n_patterns(
